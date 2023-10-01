44e9bd75db2fcfb3508ff82cb984a283b7497026
Solomon <solomonkillam@gmail.com>
2023-10-01 16:26:51 -0300

    Created input for fibonacciPrime.c

--- fibonacciPrime.c
+++ fibonacciPrime.c
@@ -1,4 +1,28 @@
-int main(void) {
+#include <stdio.h>
+#include <math.h>
+#define MAX_ARR_SIZE 100
 
+int * find_fibonacci_primes(int arr[]) {
+
+    return 0;
+}
+
+short unsigned int is_prime(unsigned int value) {
+    return 0;
+}
+
+int main(void) {
+    // Initialise relavant variables
+    int arrSize, arr[MAX_ARR_SIZE];
+
+    // Take array input
+    printf("Enter input array size: ");
+    scanf("%d", &arrSize);
+    printf("Enter values: ");
+    for (int i = 0; i < arrSize; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    find_fibonacci_primes(arr);
     return 0;
 }
